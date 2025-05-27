d02f0421421bafb8618bff06c6b0fa89ce587840
dave <dave@goodblaster.com>
2025-05-27 10:27:38 -0600

    demo update

--- demo/main.go
+++ demo/main.go
@@ -13,25 +13,24 @@ import (
 )
 
 func main() {
-	// Just use it, as console logger, with no setup.
+	// Basic usage with default console logger.
 	logos.Debug("This is a debug message")
 
-	// Include some key/values pairs.
+	// Include structured key-value fields.
 	logos.With("key2", "value2").Infof("This is an %s message", "info")
 
-	// WithError
+	// Attach an error.
 	logos.WithError(errors.New("this is an error")).Error("this is a message")
 
-	// Break off into sub-loggers.
+	// Create sub-loggers for scoped context.
 	sublog1 := logos.With("sublog", 1)
 	sublog1.Infof("This is an %s message with %s", "info", "sublog1")
 
 	sublog2 := sublog1.With("sublog", 2)
 	sublog2.Infof("This is an %s message with %s", "info", "sublog2")
-
 	sublog1.Warnf("This is a %s message with %s", "warn", "sublog1")
 
-	// Messages for all levels.
+	// Demonstrate all default levels.
 	for defLevel := logos.LevelDebug; defLevel < logos.LevelFatal; defLevel++ {
 		logos.SetLevel(defLevel)
 		logos.Print("")
@@ -41,21 +40,29 @@ func main() {
 		}
 	}
 
-	// Custom levels.
+	// LogFunc (lazy evaluation)
+	logos.LogFunc(logos.LevelDebug, func() string {
+		return expensiveOperation()
+	})
+
+	// LogIf (conditional execution)
+	logos.LogIf(logos.LevelInfo, func() {
+		fmt.Println("This log is only printed if info level is enabled")
+	})
+
+	// Custom level demonstration.
 	const (
 		LevelApple logos.Level = iota
 		LevelBanana
 		LevelCherry
 	)
 
-	// With custom names.
 	logos.LevelNames = map[logos.Level]string{
 		LevelApple:  "apple",
 		LevelBanana: "banana",
 		LevelCherry: "cherry",
 	}
 
-	// And custom console colors.
 	logos.LevelColors = map[logos.Level]logos.Color{
 		LevelApple:  logos.ColorBgGreen + logos.ColorTextBlack,
 		LevelBanana: logos.ColorBgYellow + logos.ColorTextBlack,
@@ -74,10 +81,14 @@ func main() {
 		}
 	}
 
-	// Customer formatter
+	// Custom formatter demo.
 	custom := logos.NewLogger(LevelApple, customFormatter{}, os.Stdout)
 	custom.With("key1", "value1").With("key2", "value2").Log(LevelCherry, "This is a custom message.")
-	custom.With("key1", "value1").With("key2", "value2").Log(LevelCherry, "This is a custom message.")
+}
+
+func expensiveOperation() string {
+	time.Sleep(50 * time.Millisecond)
+	return "result of expensive operation"
 }
 
 type customFormatter struct{}
@@ -90,7 +101,6 @@ func (f customFormatter) Format(level logos.Level, entry logos.Entry) string {
 	}
 	slices.Sort(tuples)
 
-	// ANSI color codes
 	textColor := logos.LevelColors[level]
 
 	line := fmt.Sprintf("%s\t%s",
